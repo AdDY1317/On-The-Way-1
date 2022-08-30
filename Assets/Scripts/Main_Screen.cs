bf621603bdf04a2f5107d9ebfa7d05aedd325233
Aditya Singh <s.addy1317@gmail.com>
2022-08-31 01:06:10 +0530

    Main_Menu_UI
    UI , BUTTONS

---
+++ Assets/Scripts/Main_Screen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Screen : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Play");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit");
+    }
+}
